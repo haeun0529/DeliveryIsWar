8a4f549e76d3ec3a85e1760b7fec9ef6cc35a08e
haeun0529 <h991512@naver.com>
2026-04-28 23:58:57 +0900

    feature : 쥐 오브젝트 추가

---
+++ Assets/Scripts/Rat.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Rat : MonoBehaviour
+{
+    public float speed = 3f;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+
+        Vector2 dir = (player.position - transform.position).normalized;
+        float randomAngle = Random.Range(-30f, 30f);
+        dir = RotateVector(dir, randomAngle);
+
+        GetComponent<Rigidbody2D>().linearVelocity = dir * speed;
+
+        Destroy(gameObject, 5f);
+    }
+
+    Vector2 RotateVector(Vector2 v, float angle)
+    {
+        float rad = angle * Mathf.Deg2Rad;
+        return new Vector2(
+            v.x * Mathf.Cos(rad) - v.y * Mathf.Sin(rad),
+            v.x * Mathf.Sin(rad) + v.y * Mathf.Cos(rad)
+        );
+    }
+}
