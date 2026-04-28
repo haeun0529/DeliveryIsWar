39de6d320e2510682d96c435b02d1fe2e58157a8
haeun0529 <h991512@naver.com>
2026-04-29 00:30:23 +0900

    feature : 목숨 UI 추가

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHp = 3;
+    private int currentHp;
+    public TextMeshProUGUI hpText;
+
+    void Start()
+    {
+        currentHp = maxHp;
+        UpdateHpUI();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Rat"))
+        {
+            currentHp--;
+            Destroy(other.gameObject);
+            UpdateHpUI();
+            Debug.Log("남은 HP: " + currentHp);
+
+            if (currentHp <= 0)
+            {
+                Debug.Log("게임 오버!");
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void UpdateHpUI()
+    {
+        string hearts = "";
+        for (int i = 0; i < currentHp; i++)
+            hearts += "♥ ";
+        for (int i = currentHp; i < maxHp; i++)
+            hearts += "♡ ";
+        hpText.text = hearts;
+    }
+}
