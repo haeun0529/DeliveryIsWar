170a9edfd2b82e60c3829ec17af6fd243ee953d4
haeun0529 <h991512@naver.com>
2026-04-28 23:56:30 +0900

    fix : 가로, 세로 사이즈 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     public float fireRate = 0.2f;
 
     private float nextFireTime = 0f;
-    private Vector3 fireOffset = new Vector3(0, 0.5f, 0); // FirePoint 대신 직접 계산
+    private Vector3 fireOffset = new Vector3(0, 0.5f, 0);
 
     void Update()
     {
@@ -25,8 +25,8 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(new Vector2(h, v) * speed * Time.deltaTime);
 
-        float clampX = Mathf.Clamp(transform.position.x, -2.5f, 2.5f);
-        float clampY = Mathf.Clamp(transform.position.y, -5f, 5f);
+        float clampX = Mathf.Clamp(transform.position.x, -4.5f, 4.5f);
+        float clampY = Mathf.Clamp(transform.position.y, -7f, 7f);
         transform.position = new Vector2(clampX, clampY);
     }
 
