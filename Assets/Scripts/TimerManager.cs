a56242412349d430cea7952c53819e0a7b1370f8
haeun0529 <h991512@naver.com>
2026-05-08 01:57:38 +0900

    fix : 4분 뒤에 보스 나오도록 수정

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -5,11 +5,15 @@ public class TimerManager : MonoBehaviour
 {
     public static TimerManager Instance;
 
-    public float bossSpawnTime = 120f;
+    public float midBossSpawnTime = 120f;
+    public float bossSpawnTime = 240f;
     public GameObject midBossPrefab;
+    public GameObject bossPrefab;
     public Image bossHpFill;
+    public Image midBossHpFill;
 
     public float currentTime = 0f;
+    private bool midBossSpawned = false;
     private bool bossSpawned = false;
 
     void Awake()
@@ -21,10 +25,38 @@ public class TimerManager : MonoBehaviour
     {
         currentTime += Time.deltaTime;
 
+        if (!midBossSpawned && currentTime >= midBossSpawnTime)
+        {
+            midBossSpawned = true;
+            MidBossSpawn();
+        }
+
         if (!bossSpawned && currentTime >= bossSpawnTime)
         {
             bossSpawned = true;
             BossSpawn();
+        }
+    }
+
+    void MidBossSpawn()
+    {
+        EnemySpawner spawner = FindObjectOfType<EnemySpawner>();
+        if (spawner != null)
+            spawner.enabled = false;
+
+        if (midBossPrefab != null)
+        {
+            Vector3 spawnPos = new Vector3(0, 8f, 0);
+            GameObject midBoss = Instantiate(midBossPrefab, spawnPos, Quaternion.identity);
+
+            if (midBossHpFill != null)
+            {
+                midBossHpFill.transform.parent.gameObject.SetActive(true);
+                MidBossController mc = midBoss.GetComponent<MidBossController>();
+                if (mc != null)
+                    mc.hpFill = midBossHpFill;
+            }
+            Debug.Log("중간 보스 등장!");
         }
     }
 
@@ -34,20 +66,19 @@ public class TimerManager : MonoBehaviour
         if (spawner != null)
             spawner.enabled = false;
 
-        if (midBossPrefab != null)
+        if (bossPrefab != null)
         {
             Vector3 spawnPos = new Vector3(0, 8f, 0);
-            GameObject boss = Instantiate(midBossPrefab, spawnPos, Quaternion.identity);
+            GameObject boss = Instantiate(bossPrefab, spawnPos, Quaternion.identity);
 
             if (bossHpFill != null)
             {
                 bossHpFill.transform.parent.gameObject.SetActive(true);
-                MidBossController midBoss = boss.GetComponent<MidBossController>();
-                if (midBoss != null)
-                    midBoss.hpFill = bossHpFill;
+                BossController bc = boss.GetComponent<BossController>();
+                if (bc != null)
+                    bc.hpFill = bossHpFill;
             }
-
-            Debug.Log("중간 보스 등장!");
+            Debug.Log("보스 등장!");
         }
     }
 }
