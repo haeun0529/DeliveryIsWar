2b3ca8f7b65e4a79ffade8677668b94b902060af
haeun0529 <h991512@naver.com>
2026-05-09 03:46:26 +0900

    feature : 게임 오버 화면 점수 UI 스크립트 추가

---
+++ Assets/Scripts/GameOverScoreDisplay.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScoreDisplay : MonoBehaviour
+{
+    public Image digit1;
+    public Image digit2;
+    public Image digit3;
+    public Image digit4;
+    public Image digit5;
+
+    public Sprite[] numberSprites; // 0~9 이미지
+
+    void Start()
+    {
+        int score = 0;
+        if (GameManager.Instance != null)
+            score = GameManager.Instance.totalScore;
+
+        score = Mathf.Clamp(score, 0, 99999);
+
+        digit1.sprite = numberSprites[score / 10000];
+        digit2.sprite = numberSprites[(score % 10000) / 1000];
+        digit3.sprite = numberSprites[(score % 1000) / 100];
+        digit4.sprite = numberSprites[(score % 100) / 10];
+        digit5.sprite = numberSprites[score % 10];
+    }
+}
