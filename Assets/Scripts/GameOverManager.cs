d028c3a189f1a2672eb938415accce75da85739f
haeun0529 <h991512@naver.com>
2026-05-09 03:47:32 +0900

    fix : 게임 오버 매니저 스크립트 수정

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,21 +1,17 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using TMPro;
 
 public class GameOverManager : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
-
     void Start()
     {
-        if (GameManager.Instance != null)
-            scoreText.text = "SCORE: " + GameManager.Instance.totalScore;
+
     }
 
     public void Retry()
     {
-        GameManager.Instance.totalScore = 0;
+        if (GameManager.Instance != null)
+            GameManager.Instance.totalScore = 0;
         SceneManager.LoadScene("Stage1");
     }
 }
